7cb31746863a5d7a98258a61b34fd640953beede
Ivan Titlov <super.titlov@inbox.ru>
2024-03-05 21:05:05 +0300

    Debug mode for app

--- Lab1-2/MainWindow.xaml.cs
+++ Lab1-2/MainWindow.xaml.cs
@@ -15,14 +15,15 @@ public partial class MainWindow : Window
     private Point _firstPoint;
     private Point _secondPoint;
     private int _pointCount;
-    
+    private bool _debugMode = false;
 
     public MainWindow()
     {
         InitializeComponent();
+        DebugModeButton.Background = Brushes.Red;
     }
 
-    private void Canvas_LeftButtonDown(object sender, MouseButtonEventArgs e)
+    private async void Canvas_LeftButtonDown(object sender, MouseButtonEventArgs e)
     {
         if (_pointCount == 0)
         {
@@ -37,60 +38,98 @@ public partial class MainWindow : Window
             if (Eclipse.IsChecked == true)
             {
                 var points = CircleBresenham.Draw(_firstPoint, _secondPoint);
-                DrawCircle(points);
+                await DrawCircle(points);
             }
 
             else if (Circle.IsChecked == true)
             {
                 var points = CircleBresenham.Draw(_firstPoint, _secondPoint, true);
-                DrawCircle(points);
+                await DrawCircle(points);
             }
             else if (ParabolaButton.IsChecked == true)
             {
                 var points = Parabola.Draw(_firstPoint, _secondPoint);
-                DrawFigure(points);
+                await DrawFigure(points);
             }
             else if (HyperbolaButton.IsChecked == true)
             {
                 var points = Hyperbola.Draw(_firstPoint, _secondPoint);
-                DrawFigure(points);
+                await DrawFigure(points);
             }
         }
     }
 
-    private void DrawFigure(List<Point> points)
+    /// <summary>
+    /// Method for drawing Parabola and Hyperbola
+    /// </summary>
+    /// <param name="points">List of points for drawing</param>
+    /// <returns></returns>
+    private async Task DrawFigure(List<Point> points)
     {
-        var polyline = new Polyline()
+        var polyline1 = new Polyline()
         {
             Stroke = Brushes.Black,
             StrokeThickness = 1
         };
-        for (var index = 2; index < points.Count-2; index+=2)
-        {
-            if (!(points[index].X >= 0) || !(points[index].X <= MainCanvas.ActualWidth) ||
-                !(points[index].Y >= 0) || !(points[index].Y <= MainCanvas.ActualHeight)) continue;
-            
-            polyline.Points.Add(points[index]);
-            polyline.Points.Add(points[index + 2]);
-        }
-        MainCanvas.Children.Add(polyline);
-        polyline = new Polyline()
+        var polyline2 = new Polyline()
         {
             Stroke = Brushes.Black,
             StrokeThickness = 1
         };
-        for (var index = 1; index < points.Count-2; index += 2)
+        if (_debugMode)
+        {
+            MainCanvas.Children.Add(polyline1);
+            MainCanvas.Children.Add(polyline2);
+        }
+        
+        for (var index = 2; index < points.Count - 2; index += 2)
         {
             if (!(points[index].X >= 0) || !(points[index].X <= MainCanvas.ActualWidth) ||
                 !(points[index].Y >= 0) || !(points[index].Y <= MainCanvas.ActualHeight)) continue;
-            
-            polyline.Points.Add(points[index]);
-            polyline.Points.Add(points[index + 2]);
+
+            polyline1.Points.Add(points[index]);
+            polyline1.Points.Add(points[index + 2]);
+
+            if (_debugMode)
+            {
+                await Task.Delay(500); // Adjust delay time as needed
+            }
         }
-        MainCanvas.Children.Add(polyline);
+
+        for (var index = 1; index < points.Count - 2; index += 2)
+        {
+            if (!(points[index].X >= 0) || !(points[index].X <= MainCanvas.ActualWidth) ||
+                !(points[index].Y >= 0) || !(points[index].Y <= MainCanvas.ActualHeight)) continue;
+
+            polyline2.Points.Add(points[index]);
+            polyline2.Points.Add(points[index + 2]);
+
+            if (_debugMode)
+            {
+                await Task.Delay(500); // Adjust delay time as needed
+            }
+        }
+
+        if (!_debugMode)
+        {
+            if (!MainCanvas.Children.Contains(polyline1))
+            {
+                MainCanvas.Children.Add(polyline1);
+            }
+
+            if (!MainCanvas.Children.Contains(polyline2))
+            {
+                MainCanvas.Children.Add(polyline2);
+            }
+        }
     }
 
-    private void DrawCircle(List<Point> points)
+    /// <summary>
+    /// Method for drawing Circle and Eclipse
+    /// </summary>
+    /// <param name="points">List of points for drawing</param>
+    /// <returns></returns>
+    private async Task DrawCircle(List<Point> points)
     {
         var polylines = new List<Polyline>();
 
@@ -101,26 +140,56 @@ public partial class MainWindow : Window
                 Stroke = Brushes.Black,
                 StrokeThickness = 1
             };
-
+            if (_debugMode)
+            {
+                MainCanvas.Children.Add(polyline);
+            }
             for (var index = i; index < points.Count; index += 4)
             {
                 if (points[index].X >= 0 && points[index].X <= MainCanvas.ActualWidth &&
                     points[index].Y >= 0 && points[index].Y <= MainCanvas.ActualHeight)
                 {
                     polyline.Points.Add(points[index]);
+                    if (_debugMode)
+                    {
+                        await Task.Delay(500);// Adjust delay time as needed
+                    }
+                    
                 }
             }
 
             polylines.Add(polyline);
+
+            
         }
 
-        foreach (var polyline in polylines)
+        if (!_debugMode)
         {
-            MainCanvas.Children.Add(polyline);
+            foreach (var polyline in polylines)
+            {
+                if (MainCanvas.Children.Contains(polyline)) continue;
+                MainCanvas.Children.Add(polyline);
+            }
         }
     }
-    private void Button_Click(object sender, RoutedEventArgs e)
+
+
+    private void ClearCanvasButton_Click(object sender, RoutedEventArgs e)
     {
         MainCanvas.Children.Clear();
+    }
+
+    private void DebugMode_ButtonClick(object sender, RoutedEventArgs e)
+    {
+        if (_debugMode is false)
+        {
+            DebugModeButton.Background = Brushes.LightGreen;
+            _debugMode = true;
+        }
+        else
+        {
+            DebugModeButton.Background = Brushes.Red;
+            _debugMode = false;
+        }
     }
 }
