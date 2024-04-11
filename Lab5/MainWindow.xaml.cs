68fb08bf5ecdf90631631868cd76bb58381247a2
Ivan Titlov <super.titlov@inbox.ru>
2024-04-11 17:07:11 +0300

    Lab5 layout

---
+++ Lab5/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckPointButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CheckIntersectionButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CheckConvexityButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CalculateNormalsButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GrahamsConvexHullButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void JarvisConvexHullButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
